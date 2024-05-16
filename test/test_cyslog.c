8e5a8f723574d2824880f5d0cdb7f4d07f78733b
barsaskn <barisaskin09@gmail.com>
2024-05-16 15:57:03 +0300

    rfc5424 tests added

--- test/test_cyslog.c
+++ test/test_cyslog.c
@@ -1,7 +1,9 @@
 #include <test_queue.h>
 #include <test_udp_server.h>
+#include <test_rfc5424_parser.h>
 
 int main() {
     run_all_queue_tests();
     run_all_udp_server_tests();
+    run_all_rfc5424_parser_tests();
 }
---
+++ test/test_rfc5424_parser.h
@@ -0,0 +1,12 @@
+#ifndef TEST_RFC5424_PARSER_TEST_H
+#define TEST_RFC5424_PARSER_TEST_H
+
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include <rfc5424_parser.h>
+
+void run_all_rfc5424_parser_tests();
+
+#endif /* TEST_RFC5424_PARSER_TEST_H*/
